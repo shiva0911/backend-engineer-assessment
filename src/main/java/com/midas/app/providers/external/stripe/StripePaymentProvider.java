b7329ec3d3850bdd3fe8b6df2b04b8448f4c38d9
shiva0911 <78194961+shiva0911@users.noreply.github.com>
2024-03-03 20:19:18 +0530

    Update StripePaymentProvider.java

--- src/main/java/com/midas/app/providers/external/stripe/StripePaymentProvider.java
+++ src/main/java/com/midas/app/providers/external/stripe/StripePaymentProvider.java
@@ -29,8 +29,58 @@ public class StripePaymentProvider implements PaymentProvider {
    * @param details is the details of the account to be created.
    * @return Account
    */
+ @RequestMapping("/createCustomer")
   @Override
-  public Account createAccount(CreateAccount details) {
-    throw new UnsupportedOperationException("Not implemented");
+  public Account createAccount(@RequestBody CreateAccount details) {
+    Map<String, Object> params = new HashMap<>();
+    params.put("name", details.getFirstName() + " " + details.getLastName());
+    params.put("email", details.getEmail());
+    try {
+      Customer customer = Customer.create(params);
+      details.setProviderId(customer.getId());
+    } catch (StripeException e) {
+      throw new RuntimeException(e);
+    }
+    Account account =
+        Account.builder()
+            .firstName(details.getFirstName())
+            .lastName(details.getLastName())
+            .email(details.getEmail())
+            .providerId(details.getProviderId())
+            .build();
+    return account;
+  }
+
+  @RequestMapping("/updateCustomer")
+  @Override
+  public Account updateAccount(@RequestBody UpdateAccount details) {
+    try {
+      Customer customer = Customer.retrieve(details.getCustomerId());
+      Map<String, Object> params = new HashMap<>();
+      String[] customerName = customer.getName().split("\\s+");
+      if (details.getFirstName() != null) {
+        params.put(
+            "name",
+            details.getFirstName() != null
+                ? details.getFirstName()
+                : customerName[0] + " " + details.getLastName() != null
+                    ? details.getLastName()
+                    : customerName[1]);
+      }
+      if (details.getEmail() != null) {
+        params.put("email", details.getEmail());
+      }
+      customer.update(params);
+    } catch (StripeException e) {
+      throw new RuntimeException(e);
+    }
+    Account account =
+        Account.builder()
+            .firstName(details.getFirstName())
+            .lastName(details.getLastName())
+            .email(details.getEmail())
+            .providerId(details.getCustomerId())
+            .build();
+    return account;
   }
 }
