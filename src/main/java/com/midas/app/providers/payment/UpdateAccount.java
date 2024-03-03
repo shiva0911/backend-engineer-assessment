b279373c6910d6bb7ea5e6e177da05ea670a09d6
shiva0911 <78194961+shiva0911@users.noreply.github.com>
2024-03-03 20:21:16 +0530

    Update UpdateAccount.java

--- src/main/java/com/midas/app/providers/payment/UpdateAccount.java
+++ src/main/java/com/midas/app/providers/payment/UpdateAccount.java
@@ -1,1 +1,16 @@
+package com.midas.app.providers.payment;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+public class UpdateAccount
+{
+	private String customerId;
+	  private String firstName;
+	  private String lastName;
+	  private String email;
+
+}
 
